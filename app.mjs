5def4947bb9e6add3aae08360c80bbfd67159e96
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-04-19 11:12:32 +0200

    Refactored pupeteer part into a ScreenshotService class

--- app.mjs
+++ app.mjs
@@ -1,65 +1,34 @@
 import express from 'express';
 import fs from 'fs';
-import md5 from 'md5';
-import path from 'path';
-import puppeteer from 'puppeteer';
 import util from 'util';
 
-const { join } = path;
+import ScreenshotService from './lib/ScreenshotService';
+
 const { promisify } = util;
 const readFile = promisify(fs.readFile);
-const exists = promisify(fs.exists);
-const stat = promisify(fs.stat);
 
 const asyncMiddleware = fn => (request, response, next) => {
   Promise.resolve(fn(request, response, next)).catch(next);
 };
 
-function getScreenshotPath(config, options) {
-  const filename = md5(JSON.stringify(options));
-  return join(config.temp, `${filename}.png`);
-}
-
-async function isScreenshotCacheValid(path, { cacheExpire }) {
-  if (! await exists(path)) {
-    return false;
-  }
-
-  const { mtimeMs } = await stat(path);
-  return Date.now() < mtimeMs + cacheExpire;
-}
-
-async function startApp(config) {
-  const { host, port, debug, logger } = config;
-
-  const browser = await puppeteer.launch();
+async function startServer(config) {
+  const { host, port, debug, logger, cacheTimeout, imageType } = config;
 
   const app = express();
+  const screenshotService = new ScreenshotService({ cacheTimeout, logger, imageType });
+  await screenshotService.start();
 
   app.get('/screenshot', asyncMiddleware(async (request, response) => {
 
     try {
       const url = 'https://example.com';
 
-      const options = { url };
-      const path = getScreenshotPath(config, options);
-      if (await isScreenshotCacheValid(path, config)) {
-        logger.log(`Reusing cached screenshot for ${JSON.stringify(options)}: ${path}`);
-      }
-      else {
-        // Open page
-        logger.log(`Opening ${url}`);
-        const page = await browser.newPage();
-        await page.goto(url);
-
-        // Take screenshot
-        logger.log(`Taking screenshot of ${url}`);
-        await page.screenshot({ path });
-      }
+      // Take screenshot
+      const path = await screenshotService.takeScreenshot({ url });
 
       // Write response
       const img = await readFile(path);
-      response.writeHead(200, { 'Content-Type': 'image/png' });
+      response.writeHead(200, { 'Content-Type': `image/${imageType}` });
       if (config.cors) {
         response.setHeader('Access-Control-Allow-Origin', '*');
         response.setHeader('Access-Control-Expose-Headers', 'Content-Type');
@@ -70,27 +39,48 @@ async function startApp(config) {
       response.writeHead(500);
       const body = { error: e.message };
       if (debug) {
-        body.trace = e.trace;
+        body.stack = e.stack.split('\n');
       }
-      response.end(JSON.stringify(body));
+      response.end(JSON.stringify(body, null, debug ? 2 : 0));
     }
   }));
 
   // Start listening
-  app.listen(port, host, () => {
+  const server = app.listen(port, host, () => {
     logger.log(`Server listening on ${host}:${port}${debug ? ' with debugging mode enabled' : ''}`);
   });
 
-  return { app, browser };
+  return { app, server, screenshotService };
 }
 
 const env = process.env.NODE_ENV || 'development';
 
-startApp({
+let server, screenshotService;
+
+startServer({
   port: process.env.PORT || 8000,
   host: process.env.HOST || '0.0.0.0',
   debug: env === 'development' || false,
-  temp: '/tmp',
   logger: console,
-  cacheExpire: env === 'development' ? 5000 : 3600000
+  cacheTimeout: env === 'development' ? 5000 : 3600000,
+  imageType: 'jpeg',
+}).then(instances => {
+  server = instances.server;
+  screenshotService = instances.screenshotService;
 });
+
+async function stopApp() {
+  if (screenshotService) {
+    try {
+      await screenshotService.stop();
+      console.log('Stopped screenshot service gracefully');
+      process.exit(0);
+    } catch(error) {
+      console.error(`Could not stop screenshot service gracefully: ${error}`);
+      process.exit(1);
+    }
+  }
+}
+
+process.on('SIGTERM', stopApp);
+process.on('SIGINT', stopApp);
